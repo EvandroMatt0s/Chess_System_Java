24fc2d5ede119d82a72fbbb9ea2a192235e7cfc3
Evandro Mattos <emattos0114@outlook.com>
2022-04-04 19:52:04 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	protected Board bord;
+	
+	public Piece(Board bord) {
+		this.bord = bord;
+	}
+	protected Board getBord() {
+		return bord;
+	}
+	 
+	
+	
+	
+	
+	
+	
+	
+}
